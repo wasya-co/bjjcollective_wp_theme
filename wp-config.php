0e0e9e76ec483d7b1e979db16bf38633a60136fd
v p <piousbox@vs-MacBook-Pro.local>
2018-09-09 15:27:38 -0500

    looks good, groking for bjjc

--- wp-config.php
+++ wp-config.php
@@ -29,10 +29,10 @@ define('DB_USER', 'root');
 define('DB_PASSWORD', 'awesome_password');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'host.docker.internal:3306');
 
 /** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8mb4');
+define('DB_CHARSET', 'utf8');
 
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
